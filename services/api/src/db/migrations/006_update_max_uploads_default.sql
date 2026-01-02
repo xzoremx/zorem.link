57e9ad381981cb216a3af2f9881322ea8d5f6bf9
xzoremx <renato.huamani@pucp.edu.pe>
2026-01-01 23:15:27 -0500

    test

--- services/api/src/db/migrations/006_update_max_uploads_default.sql
+++
@@ -1,8 +0,0 @@
--- Migration 006: Update max_uploads_per_viewer default to 1
--- Changes the default value from 3 to 1
-
--- Update existing rooms with default 3 to 1 (if they still have the old default)
-UPDATE rooms SET max_uploads_per_viewer = 1 WHERE max_uploads_per_viewer = 3;
-
--- Change the default value for future inserts
-ALTER TABLE rooms ALTER COLUMN max_uploads_per_viewer SET DEFAULT 1;
